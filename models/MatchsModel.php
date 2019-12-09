61ed6d1d60ce37c2296539bfd8022d7871585b52
Tony Blard <tonyblard55@gmail.com>
2019-12-09 15:54:39 +0100

    ranking done

--- models/MatchsModel.php
+++ models/MatchsModel.php
@@ -56,75 +56,75 @@ class MatchsModel extends Model
   }
 
   /**
-  *@return array[EntityMatch]
+  *@return array
   */
   public function getRank(): array
   {
     $request =
-"SELECT  @rank := @rank + 1 as '#', q.*
-FROM (
-SELECT teams.name, teams.id, gp, score_for gf, score_against ga, score_for - score_against gd, w, l, d, (w * 3) + d as pts
-  FROM  teams
-    inner join
-      (select team, sum(score_home) score_for
-         from
-           (select id_team_home as team, score_home
-              from matchs
-            WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
-            union all
-            select id_team_away, score_away
-              from matchs
-            WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
-           ) qq
-         group by team
-       ) q1
-       on teams.id = q1.team
-    inner join
-      (select id_team_home as team, sum(score_away) score_against
-         from
-           (select id_team_home, score_away
-              from matchs
-            WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
-            union all
-            select id_team_away, score_home
-              from matchs
-            WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
-           ) qq
-        group by team
-     ) q2
-     on teams.id = q2.team
-   inner join
-     (SELECT id_team_home as team, count(case when result = 1 then result end) w, count(case when result = 0 then result end) d, count(case when result = -1 then result end) l
-        FROM
-            (SELECT m.id_team_home, case when m.score_home > m.score_away then 1 when m.score_home = m.score_away then 0 else -1 end result
-             FROM matchs AS m
-             WHERE m.score_home IS NOT NULL AND m.score_away IS NOT NULL
-             UNION ALL SELECT m.id_team_away, case when m.score_away > m.score_home then 1 when m.score_away = m.score_home then 0 else -1 end result
-             FROM matchs AS m
-             WHERE m.score_home IS NOT NULL AND m.score_away IS NOT NULL ) qq
-             GROUP BY team
-     ) q3
-     on teams.id = q3.team
-    inner join
-     (select team, count(*) gp
-       from
-        (select id_team_home as team
-           from matchs
-         WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
-         union all
-         select id_team_away as team
-           from matchs
-         WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
-        ) qq
-       GROUP BY team
-     ) q4
-     ON teams.id = q4.team
+    "SELECT  @rank := @rank + 1 as '#', q.*
+    FROM (
+    SELECT teams.name, teams.id, gp, score_for gf, score_against ga, score_for - score_against gd, w, l, d, (w * 3) + d as pts
+      FROM  teams
+        inner join
+          (select team, sum(score_home) score_for
+             from
+               (select id_team_home as team, score_home
+                  from matchs
+                WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
+                union all
+                select id_team_away, score_away
+                  from matchs
+                WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
+               ) qq
+             group by team
+           ) q1
+           on teams.id = q1.team
+        inner join
+          (select id_team_home as team, sum(score_away) score_against
+             from
+               (select id_team_home, score_away
+                  from matchs
+                WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
+                union all
+                select id_team_away, score_home
+                  from matchs
+                WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
+               ) qq
+            group by team
+         ) q2
+         on teams.id = q2.team
+       inner join
+         (SELECT id_team_home as team, count(case when result = 1 then result end) w, count(case when result = 0 then result end) d, count(case when result = -1 then result end) l
+            FROM
+                (SELECT m.id_team_home, case when m.score_home > m.score_away then 1 when m.score_home = m.score_away then 0 else -1 end result
+                 FROM matchs AS m
+                 WHERE m.score_home IS NOT NULL AND m.score_away IS NOT NULL
+                 UNION ALL SELECT m.id_team_away, case when m.score_away > m.score_home then 1 when m.score_away = m.score_home then 0 else -1 end result
+                 FROM matchs AS m
+                 WHERE m.score_home IS NOT NULL AND m.score_away IS NOT NULL ) qq
+                 GROUP BY team
+         ) q3
+         on teams.id = q3.team
+        inner join
+         (select team, count(*) gp
+           from
+            (select id_team_home as team
+               from matchs
+             WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
+             union all
+             select id_team_away as team
+               from matchs
+             WHERE matchs.score_home IS NOT NULL AND matchs.score_away IS NOT NULL
+            ) qq
+           GROUP BY team
+         ) q4
+         ON teams.id = q4.team
 
-   ORDER BY pts DESC, gd DESC
-) q, (select @rank := 0) z";
+       ORDER BY pts DESC, gd DESC, gf DESC
+    ) q, (select @rank := 0) z";
     $stmt = $this->db->prepare($request);
     $stmt->execute();
-    return $stmt->fetchAll(\PDO::FETCH_CLASS, 'Entities\Match');
+    return $stmt->fetchAll(\PDO::FETCH_CLASS);
   }
 
 
--- views/ranking.php
+++ views/ranking.php
@@ -1,3 +1,43 @@
 <?php $extend = 'public/index.php'; ?>
 <?php $title = "Classement ligue 1"?>
 <?php $description = "Retrouvez le classement de ligue 1"; ?>
+
+<style>
+  .team {
+    width: 40rem;
+  }
+</style>
+
+<div class="container">
+  <div class="row">
+    <table class="table  table-borderless">
+    <tbody>
+      <tr>
+        <th class="team"></th>
+        <td>Pts</td>
+        <td>J.</td>
+        <td>G.</td>
+        <td>N.</td>
+        <td>P.</td>
+        <td>p.</td>
+        <td>c.</td>
+        <td>+/-</td>
+      </tr>
+      <?php foreach ($ranks as $rank): ?>
+        <tr>
+          <th class="team"><?php echo $rank->name; ?></th>
+          <th><?php echo $rank->pts; ?></th>
+          <td><?php echo $rank->gp; ?></td>
+          <td><?php echo $rank->w; ?></td>
+          <td><?php echo $rank->d; ?></td>
+          <td><?php echo $rank->l; ?></td>
+          <td><?php echo $rank->gf; ?></td>
+          <td><?php echo $rank->ga ;?></td>
+          <td><?php echo $rank->gd; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
+  </div>
+</div>
